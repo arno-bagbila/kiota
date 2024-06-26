54352c1e2722c5ab2acd7defc32341405d957f5b
Arnaud Bagbila <beat4free@gmail.com>
2024-06-26 19:54:16 +0100

    first commit

---
+++ lexis/Program.cs
@@ -0,0 +1,58 @@
+﻿using Lexis.Client;
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+
+var authProvider = new AnonymousAuthenticationProvider();
+var adapter = new HttpClientRequestAdapter(authProvider);
+var client = new LexisClient(adapter);
+
+try
+{
+    // GET /users/
+    var users = await client.Api.Users.GetAsync();
+    Console.WriteLine($"Retrieved {users?.Count} users.");
+
+    // GET /users/{id}
+    var user = await client.Api.Users[users?[0].Id].GetAsync();
+    Console.WriteLine($"Retrieved user {user?.FirstName}.");
+
+    // POST /users/
+    var newUser = new Lexis.Client.Models.CreateUser
+    {
+        FirstName = "John",
+        LastName = "Doe"
+
+    };
+    var createdUser = await client.Api.Users.PostAsync(newUser);
+    Console.WriteLine($"Created user {createdUser?.FirstName}.");
+
+    // GET /blogs/
+    var blogs = await client.Api.Blogs.GetAsync();
+    Console.WriteLine($"Retrieved {blogs?.Count} blogs.");
+
+    // GET /blogs/{id}
+    var blog = await client.Api.Blogs[blogs?[0].Id].GetAsync();
+    Console.WriteLine($"Retrieved blog {blog?.Text}.");
+
+    // GET /blogs/authors/{id}
+    var authorBlogs = await client.Api.Blogs.Author[users?[0].Id].GetAsync();
+    Console.WriteLine($"Retrieved {authorBlogs?.Count} blogs by author {users?[0].FirstName} {users?[0].LastName}.");
+
+    // POST /blogs
+    var newBlog = new Lexis.Client.Models.CreateBlog
+    {
+        Text = "Hello, World!",
+        AuthorId = users?[0].Id,
+        PublishedOn = DateTimeOffset.Now.AddHours(1),
+        Category = "General"
+    };
+    var createdBlog = await client.Api.Blogs.PostAsync(newBlog);
+    Console.WriteLine($"Created blog {createdBlog?.Text}.");
+
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+    Console.WriteLine(ex.StackTrace);
+}
